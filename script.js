1fe29f018c94aec0626c6cf4b6250820a98f4a91
theSaviKing <tpaulkariuki@gmail.com>
2022-09-12 00:40:01 -0500

    fixed 12-hour

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const qs = window.location.search;
 const up = new URLSearchParams(qs);
 var miltime = true;
-if (up.has('hour') && up.get('hour') == 12) {
+if (up.has('hour') && up.get('hour') == 12 || up.has('ampm') || up.has('12hour')) {
     miltime = false;
 }
 
@@ -14,22 +14,27 @@ document.getElementById("date").innerHTML = `${dotw[d.getDay()]} &mdash; <span c
 
 /**********/
 
-function getTime() {
+function getTime24() {
     const t = new Date();
-    if (!miltime && t.getHours() > 12) {
-        document.getElementById("hours").textContent = t.getHours() - 12
-    } else {
-        document.getElementById("hours").textContent = t.getHours()
-    }
+    document.getElementById("hours").textContent = t.getHours()
     document.getElementById("minutes").textContent = String(t.getMinutes()).padStart(2, "0")
     document.getElementById("seconds").textContent = String(t.getSeconds()).padStart(2, "0")
-    if (!miltime) {
-        if (t.getHours() > 12) {
-            document.getElementById("seconds").textContent += " AM"
-        } else {
-            document.getElementById("seconds").textContent += " PM"
-        }
-    }
 }
+
+function getTime12() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds + " " + ampm;
+}
+var getTime = miltime ? getTime24 : getTime12
 getTime()
 setInterval(getTime, 500);
